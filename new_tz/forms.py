f3ee9459c8e9c5020e3454abce24f1ce8a7fc851
korsunowk <korsunowk@yandex.ua>
2016-06-30 09:41:42 +0300

    Update forms.py

--- new_tz/forms.py
+++ new_tz/forms.py
@@ -1,5 +1,27 @@
 from django import forms
 
 class Form(forms.Form):
-    text = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Введите текст','required':'required','id':"text1",'rows':"16",'cols':"100"}))
-    key = forms.IntegerField(required=True,min_value=0,max_value=25, widget=forms.NumberInput(attrs={'required':'required','id':'key','class':"rot_inp"}))
+    text = forms.CharField(
+        required=True, 
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Введите текст',
+                'required':'required',
+                'id':"text1",
+                'rows':"16",
+                'cols':"100"
+                }
+            )
+        )
+    key = forms.IntegerField(
+        required=True,
+        min_value=0,
+        max_value=25, 
+        widget=forms.NumberInput(
+            attrs={
+                'required':'required',
+                'id':'key',
+                'class':"rot_inp"
+                }
+            )
+        )
