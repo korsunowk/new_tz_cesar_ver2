37370a0b142545d5a64f48527a6e435a05d831f9
Korsunow Konstantin <korsunowk@yandex.ua>
2016-10-03 12:09:17 +0300

    Code upgraded with CBV

--- new_tz/urls.py
+++ new_tz/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^encrypt/$', views.encrypt),
-    url(r'^decrypt/$', views.decrypt),
-    url(r'^find/$', views.find)
+    url(r'^encrypt/$', views.CesarCryptingView().encrypt),
+    url(r'^decrypt/$', views.CesarCryptingView().decrypt),
+    url(r'^find/$', views.CesarCryptingView().find)
 ]
--- new_tz/views.py
+++ new_tz/views.py
@@ -2,56 +2,56 @@ from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from CesarCrypt.CesarCrypting import CesarCrypting
+from django.views.generic import View
 
 
 cesarCrypting = CesarCrypting()
 
 
-@csrf_exempt
-def encrypt(request):
-    text = request.POST.get('text', False)
-    key = int(request.POST.get('key', False))
+class CesarCryptingView(View):
+    @staticmethod
+    def encrypt(request):
+        text = request.POST.get('text', False)
+        key = int(request.POST.get('key', False))
 
-    if text and key:
-        return JsonResponse({
-            'finaltext': cesarCrypting.encrypting(text, key)
-        })
-    else:
-        return redirect('/')
-
-
-@csrf_exempt
-def decrypt(request):
-    text = request.POST.get('text', False)
-    key = int(request.POST.get('key', False))
-    if text and key:
-        return JsonResponse({
-            'finaltext': cesarCrypting.decrypting(text, key)
-        })
-    else:
-        return redirect('/')
-
-
-@csrf_exempt
-def find(request):
-    mytext = request.POST.get('mytext', False)
-    if mytext:
-        if cesarCrypting.find_text(mytext):
-            return JsonResponse(
-                {
-                    'enc': 'false'
-                }
-            )
+        if text and key:
+            return JsonResponse({
+                'finaltext': cesarCrypting.encrypting(text, key)
+            })
         else:
-            for i in range(1, 27):
-                if cesarCrypting.find_text(cesarCrypting.decrypting(mytext, i)):
-                    return JsonResponse(
-                        {
-                            'enc': 'true',
-                            'word': cesarCrypting.decrypting(mytext, i),
-                            'key': i
-                        }
-                    )
-            return JsonResponse({'enc': 'dont_know'})
-    else:
-        redirect('/')
+            return redirect('/')
+
+    @staticmethod
+    def decrypt(request):
+        text = request.POST.get('text', False)
+        key = int(request.POST.get('key', False))
+        if text and key:
+            return JsonResponse({
+                'finaltext': cesarCrypting.decrypting(text, key)
+            })
+        else:
+            return redirect('/')
+
+    @staticmethod
+    def find(request):
+        mytext = request.POST.get('mytext', False)
+        if mytext:
+            if cesarCrypting.find_text(mytext):
+                return JsonResponse(
+                    {
+                        'enc': 'false'
+                    }
+                )
+            else:
+                for i in range(1, 27):
+                    if cesarCrypting.find_text(cesarCrypting.decrypting(mytext, i)):
+                        return JsonResponse(
+                            {
+                                'enc': 'true',
+                                'word': cesarCrypting.decrypting(mytext, i),
+                                'key': i
+                            }
+                        )
+                return JsonResponse({'enc': 'dont_know'})
+        else:
+            redirect('/')
